f5522053c2ead386528134d814a77bb2c97b3c58
AlexandroAR <120469655+AlexandroAR@users.noreply.github.com>
2022-12-13 16:26:38 +0100

    Add files via upload

--- main/TraitementFichiers.py
+++ main/TraitementFichiers.py
@@ -2,30 +2,49 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import ap
 
-def ExtractionDesAP(fichier):
+def ExtractionDesAP(fichier_ap, fichier_apc):
     aps = []
-    with open(fichier, 'r') as f:
+    apcs = []
+    with open(fichier_ap, 'r') as f:
         lignes = f.readlines()
+
+    with open(fichier_apc, 'r') as f:
+        apcs = f.readline().strip().split(' ')
+
     for x in lignes:
         x.strip()
-        index = int(x[:2])
-        rayon = int(x[-3:])
+        index = x[:2].strip()
+        rayon = x[-3:].strip()
+
         coord = x[x.find('(')+1:x.find(')')]
         coord = coord.split(",")
         coord = (int(coord[0]), int(coord[1]))
-        a = ap.AP(index, coord, rayon)
+
+        if index in apcs:
+            couleur = 'blue'
+            type_ap = 'C'
+        else:
+            couleur = 'red'
+            type_ap = 'S'
+
+        a = ap.AP(int(index), coord, int(rayon), couleur, type_ap)
         aps.append(a)
+        print(a)
     return aps
 
 def topologie():
     topo = nx.Graph()
-    for x in ExtractionDesAP("Test/test_AP.txt"):
-        topo.add_node(x.index, coord=x.coord)
+    for x in ExtractionDesAP("Test/test_AP.txt", "Test/test_APC.txt"):
+        topo.add_node(x.index, coord=x.coord, couleur=x.color)
     return topo
 
 def affichage(graphe):
+    colors = []
     pos = nx.get_node_attributes(graphe,'coord')
-    nx.draw_networkx_nodes(graphe, pos)
+    col = nx.get_node_attributes(graphe, 'couleur')
+    for x in col:
+        colors.append(col[x])
+    nx.draw_networkx_nodes(graphe, pos, node_color=colors)
     nx.draw_networkx_labels(graphe, pos)
     plt.show()
 
--- main/ap.py
+++ main/ap.py
@@ -1,8 +1,10 @@
 class AP():
-    def __init__(self, index=0, coord=(0, 0) , rayon=0):
+    def __init__(self, index=0, coord=(0, 0) , rayon=0, color='white', type_ap = 'S'):
         self.index = index
         self.coord = coord
         self.rayon = rayon
+        self.color = color
+        self.type_ap = type_ap
 
     def __str__(self):
-        return f"AP n°{self.index}\nCoordonnées: {self.coord}\nRayon de coverture: {self.rayon}"
+        return f"AP n°{self.index}\nCoordonnées: {self.coord}\nRayon de coverture: {self.rayon}\nCouleur: {self.color}\nType: {self.type_ap}"
