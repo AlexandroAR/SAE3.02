d4493d107ce2ef14e1327621c38de9702d14e7c6
AlexandroAR <120469655+AlexandroAR@users.noreply.github.com>
2022-12-16 11:35:51 +0100

    Add files via upload

--- Test/generateur.py
+++ Test/generateur.py
@@ -3,22 +3,24 @@ import random as rd
 def gen_ligne(nbr_lignes):
     aps = []
     for i in range(1, nbr_lignes+1):
-        ligne = f"{i} ({rd.randint(-200, 200)}, {rd.randint(-200, 200)}) {rd.randint(10, 30)}"
+        ligne = f"{i} ({rd.randint(-200, 200)}, {rd.randint(-200, 200)}) {rd.randint(10, 50)}"
         aps.append(ligne)
     return aps
 
 def gen_fichier(longeur):
     aps = gen_ligne(longeur)
-    with open("test_AP.txt", 'w') as f:
+    with open("Test/test_AP.txt", 'w') as f:
         for ap in aps:
             f.write(f'{ap}\n')
 
-    with open("test_APC.txt", 'w') as f:
+    apcs = []
+    with open("Test/test_APC.txt", 'w') as f:
         for x in range(0, (len(aps)//10)):
             apc = rd.randint(1, len(aps))
-            f.write(f"{apc} ")
+            if apc not in apcs:
+                apcs.append(apc)
+                f.write(f"{apc} ")
 
 
 gen_fichier(50)
-
 
